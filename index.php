c737455f892352d6fe21fb20a4e0c98746495a0c
LPThibert-CA <57185578+LPThibert-CA@users.noreply.github.com>
2022-08-10 12:20:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello from LP\'s World</p>'; ?> 
+ <?php echo '<p>Hello from LP\'s World</p>'; ?>
+ <?php echo '<p>It\'s a working world.</p>'; ?> 
  </body>
 </html>
