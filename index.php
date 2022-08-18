33db6a6b8f581e62ae1645ed8612e68e86bf9420
LPThibert-CA <57185578+LPThibert-CA@users.noreply.github.com>
2022-08-18 16:43:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
  <?php echo '<p>It\'s a better working world.</p>'; ?> 
  <?php echo '<p>Fingers crossed.</p>'; ?> 
  <?php echo '<p>Another good test.</p>'; ?> 
+ <?php echo '<p>Last try :).</p>'; ?> 
  </body>
 </html>
